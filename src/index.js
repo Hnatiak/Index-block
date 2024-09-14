a45c0cb8ae74590074afb9b591d1bb29b4ff11f0
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-09-14 22:49:25 +0200

    Index block v1.1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
+import { BrowseRouter } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowseRouter basename="/Index-block/">
+      <App />
+    </BrowseRouter>
   </React.StrictMode>
 );
 
